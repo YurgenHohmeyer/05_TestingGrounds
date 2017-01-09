d3d183d42a473bb2bff66a4fc0a4afc4be56fccc
YurgenHohmeyer <yHohmeyer@gmail.com>
2017-01-09 01:13:21 -0500

    Getting Blackboard Index in C++

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,11 +2,15 @@
 
 #include "S05_TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
 
-EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++ - yay!"));
 	return EBTNodeResult::Succeeded;
 }
